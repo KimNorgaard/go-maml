868da9f6bd10fbfb954ecd378dc0b01498ed72a2
Kim Nørgaard <jasen@jasen.dk>
2025-09-30 13:27:12 +0200

    feat: add happy path tests for parsing basic types

--- parser_test.go
+++ parser_test.go
@@ -1,45 +1,131 @@
 package maml
 
 import (
-	"flag"
-	"os"
-	"path/filepath"
-	"strings"
 	"testing"
 
+	"github.com/KimNorgaard/go-maml/ast"
 	"github.com/stretchr/testify/require"
 )
 
-var update = flag.Bool("update", false, "update golden files")
+func TestIdentifierExpression(t *testing.T) {
+	input := `foobar`
 
-func TestParser(t *testing.T) {
-	files, err := filepath.Glob("testdata/*.maml")
-	require.NoError(t, err)
+	l := NewLexer([]byte(input))
+	p := NewParser(l)
+	doc := p.ParseDocument()
+	require.Empty(t, p.Errors(), "parser has errors")
 
-	for _, file := range files {
-		t.Run(file, func(t *testing.T) {
-			src, err := os.ReadFile(file)
-			require.NoError(t, err)
+	require.Len(t, doc.Statements, 1, "doc.Statements does not contain 1 statement")
 
-			node, err := Parse(src)
+	stmt, ok := doc.Statements[0].(*ast.ExpressionStatement)
+	require.True(t, ok, "doc.Statements[0] is not ast.ExpressionStatement")
 
-			var actual string
-			if err != nil {
-				actual = err.Error()
-			} else {
-				actual = node.String()
-			}
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	require.True(t, ok, "exp not *ast.Identifier")
 
-			goldenFile := strings.Replace(file, ".maml", ".golden", 1)
-			if *update {
-				err := os.WriteFile(goldenFile, []byte(actual), 0o644)
-				require.NoError(t, err)
-			}
+	require.Equal(t, "foobar", ident.Value, "ident.Value not %s", "foobar")
+	require.Equal(t, "foobar", ident.TokenLiteral(), "ident.TokenLiteral not %s", "foobar")
+}
 
-			expected, err := os.ReadFile(goldenFile)
-			require.NoError(t, err)
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := `5`
 
-			require.Equal(t, string(expected), actual, "parser output does not match golden file")
-		})
+	l := NewLexer([]byte(input))
+	p := NewParser(l)
+	doc := p.ParseDocument()
+	require.Empty(t, p.Errors(), "parser has errors")
+
+	require.Len(t, doc.Statements, 1, "doc.Statements does not contain 1 statement")
+
+	stmt, ok := doc.Statements[0].(*ast.ExpressionStatement)
+	require.True(t, ok, "doc.Statements[0] is not ast.ExpressionStatement")
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	require.True(t, ok, "exp not *ast.IntegerLiteral")
+
+	require.Equal(t, int64(5), literal.Value, "literal.Value not %d", 5)
+	require.Equal(t, "5", literal.TokenLiteral(), "literal.TokenLiteral not %s", "5")
+}
+
+func TestBooleanLiteralExpression(t *testing.T) {
+	tests := []struct {
+		input         string
+		expectedValue bool
+	}{
+		{"true", true},
+		{"false", false},
 	}
+
+	for _, tt := range tests {
+		l := NewLexer([]byte(tt.input))
+		p := NewParser(l)
+		doc := p.ParseDocument()
+		require.Empty(t, p.Errors(), "parser has errors")
+
+		require.Len(t, doc.Statements, 1, "doc.Statements does not contain 1 statement")
+
+		stmt, ok := doc.Statements[0].(*ast.ExpressionStatement)
+		require.True(t, ok, "doc.Statements[0] is not ast.ExpressionStatement")
+
+		literal, ok := stmt.Expression.(*ast.BooleanLiteral)
+		require.True(t, ok, "exp not *ast.BooleanLiteral")
+
+		require.Equal(t, tt.expectedValue, literal.Value, "literal.Value not %t", tt.expectedValue)
+	}
+}
+
+func TestStringLiteralExpression(t *testing.T) {
+	input := `"hello world"`
+
+	l := NewLexer([]byte(input))
+	p := NewParser(l)
+	doc := p.ParseDocument()
+	require.Empty(t, p.Errors(), "parser has errors")
+
+	require.Len(t, doc.Statements, 1, "doc.Statements does not contain 1 statement")
+
+	stmt, ok := doc.Statements[0].(*ast.ExpressionStatement)
+	require.True(t, ok, "doc.Statements[0] is not ast.ExpressionStatement")
+
+	literal, ok := stmt.Expression.(*ast.StringLiteral)
+	require.True(t, ok, "exp not *ast.StringLiteral")
+
+	require.Equal(t, "hello world", literal.Value, "literal.Value not %s", "hello world")
+}
+
+func TestFloatLiteralExpression(t *testing.T) {
+	input := `1.23`
+
+	l := NewLexer([]byte(input))
+	p := NewParser(l)
+	doc := p.ParseDocument()
+	require.Empty(t, p.Errors(), "parser has errors")
+
+	require.Len(t, doc.Statements, 1, "doc.Statements does not contain 1 statement")
+
+	stmt, ok := doc.Statements[0].(*ast.ExpressionStatement)
+	require.True(t, ok, "doc.Statements[0] is not ast.ExpressionStatement")
+
+	literal, ok := stmt.Expression.(*ast.FloatLiteral)
+	require.True(t, ok, "exp not *ast.FloatLiteral")
+
+	require.Equal(t, float64(1.23), literal.Value, "literal.Value not %f", 1.23)
+	require.Equal(t, "1.23", literal.TokenLiteral(), "literal.TokenLiteral not %s", "1.23")
+}
+
+func TestNullLiteralExpression(t *testing.T) {
+	input := `null`
+
+	l := NewLexer([]byte(input))
+	p := NewParser(l)
+	doc := p.ParseDocument()
+	require.Empty(t, p.Errors(), "parser has errors")
+
+	require.Len(t, doc.Statements, 1, "doc.Statements does not contain 1 statement")
+
+	stmt, ok := doc.Statements[0].(*ast.ExpressionStatement)
+	require.True(t, ok, "doc.Statements[0] is not ast.ExpressionStatement")
+
+	_, ok = stmt.Expression.(*ast.NullLiteral)
+	require.True(t, ok, "exp not *ast.NullLiteral")
 }
